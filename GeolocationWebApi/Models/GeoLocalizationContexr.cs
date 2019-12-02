39636f5301b322c4e8e96f4bf21021fd53c58ac0
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-12-02 07:03:23 +0100

    Add database context class

---
+++ GeolocationWebApi/Models/GeoLocalizationContexr.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace GeolocationWebApi.Models
+{
+    public class GeoLocalizationContexr : DbContext
+    {
+        private static string connectionString = @"Data Source .; Initial Catalog=Test;Integrated Seciurity =True";
+
+        public GeoLocalizationContexr() : base(connectionString) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            System.Data.Entity.Database.SetInitializer<GeoLocalizationContexr>(new CreateDatabaseIfNotExists<GeoLocalizationContexr>());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<GeoLocalization> GeoLocalizations { get; set; }
+    }
+}
