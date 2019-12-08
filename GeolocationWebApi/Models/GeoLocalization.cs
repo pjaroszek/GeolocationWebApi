8a91240b34c56b877706d560678bb4b54bd9db31
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-12-08 20:01:42 +0100

    add description to the class and property

--- GeolocationWebApi/Models/GeoLocalization.cs
+++ GeolocationWebApi/Models/GeoLocalization.cs
@@ -1,17 +1,47 @@
 ﻿namespace GeolocationWebApi.Models
-{
+{/// <summary>
+/// Geolocalization details
+/// </summary>
     public class GeoLocalization
     {
         public int id { get; set; }
+        /// <summary>
+        /// IP Adress
+        /// </summary>
         public string ip { get; set; }
+        /// <summary>
+        /// URL Adress
+        /// </summary>
         public string url { get; set; }
+        /// <summary>
+        /// City name
+        /// </summary>
         public string city { get; set; }
+        /// <summary>
+        /// Region name
+        /// </summary>
         public string region { get; set; }
+        //country code
         public string country { get; set; }
+        /// <summary>
+        /// geographic localization
+        /// </summary>
         public string loc { get; set; }
+        /// <summary>
+        /// Organmization, company name
+        /// </summary>
         public string org { get; set; }
+        /// <summary>
+        /// Postal code
+        /// </summary>
         public string postal { get; set; }
+        /// <summary>
+        /// Time zone
+        /// </summary>
         public string timezone { get; set; }
+        /// <summary>
+        /// description - readme
+        /// </summary>
         public string readme { get; set; }
     }
 }
